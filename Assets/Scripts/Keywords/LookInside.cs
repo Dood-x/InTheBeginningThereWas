d03ac79823f37e53b7d74ff6715dcab9e3388e24
Dood-x <Dood-x@users.noreply.github.com>
2020-01-14 21:20:23 +0100

    added more keywords

---
+++ Assets/Scripts/Keywords/LookInside.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/InputActions/LookInside", order = 1)]
+public class LookInside : InputAction
+{
+    public override void RespondToInput(GameController controller, string objectKeyword)
+    {
+        controller.sceneNavigation.AttemptToChangeScenes(objectKeyword);
+    }
+}
