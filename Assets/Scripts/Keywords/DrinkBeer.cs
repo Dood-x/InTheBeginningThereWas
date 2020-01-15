9ad1a2b05076aef003f7f37f84d6be3d527c0aeb
Dood-x <Dood-x@users.noreply.github.com>
2020-01-15 08:34:54 +0100

    fixed drink bug

--- Assets/Scripts/Keywords/DrinkBeer.cs
+++ Assets/Scripts/Keywords/DrinkBeer.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/InputActions/Drink", order = 1)]
-public class Drink : InputAction
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/InputActions/DrinkBeer", order = 1)]
+public class DrinkBeer : InputAction
 {
     public override void RespondToInput(GameController controller, string objectKeyword)
     {
